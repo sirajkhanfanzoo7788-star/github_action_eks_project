09d0ff456c5a7a1650a0665d938c620f6614bdc9
Siraj Ahmad <sirajkhanfanzoo7788@gmail.com>
2026-02-15 16:34:45 +0500

    EIGHT   commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("GitHub Actions + ECR + EKS Working 🚀");
+  res.send("GitHub Actions + ECR + EKS Working 🚀 ALUMDULILLAH");
 });
 
 app.listen(3000, () => {
