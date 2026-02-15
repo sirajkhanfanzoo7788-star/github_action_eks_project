ce900de9644972c69c3dcfbebc2fae3a3b27299f
Siraj Ahmad <sirajkhanfanzoo7788@gmail.com>
2026-02-15 15:00:50 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("GitHub Actions + ECR + EKS Working 🚀");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
